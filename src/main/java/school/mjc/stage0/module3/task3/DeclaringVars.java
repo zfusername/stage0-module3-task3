3114e8f0b43f743de9e2a4e0bd4cd1c354c403e7
zfusername <farrukh_ziyaev@epam.com>
2023-07-13 11:59:05 +0500

    changed

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -3,5 +3,10 @@ package school.mjc.stage0.module3.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
+        int a;
+        int b;
+        int c;
+        a = b = c = 10;
+        System.out.println(c);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -3,5 +3,22 @@ package school.mjc.stage0.module3.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        int linkToFirst = first, linkToSecond = second
+                , linkToThird = third;
+        first = 15;
+        second = 6;
+        third = 4;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
