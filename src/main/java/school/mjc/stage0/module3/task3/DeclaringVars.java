6e6432b91b5e68161094cfb338181bc181f5426f
zfusername <farrukh_ziyaev@epam.com>
2023-07-13 12:08:57 +0500

    changed2

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -3,9 +3,7 @@ package school.mjc.stage0.module3.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
-        int a;
-        int b;
-        int c;
+        int a,b,c;
         a = b = c = 10;
         System.out.println(c);
     }
